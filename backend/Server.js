ef05582e8f2708969a7cdf5bcccdb548b54a294a
Saran-k-ece <saran.k2022ece@sece.ac.in>
2025-08-07 23:45:56 +0530

    Update9

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const app = express()
 
 // ✅ CORS (Fixed)
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://ecommerce-4vew.vercel.app'], 
+    origin: ['http://localhost:5173', 'https://ecommerce-dusky-six.vercel.app'], 
     credentials: true
 }))
 
